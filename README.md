3b4c3ddad23b1122c9a3cdd76dc24928afccf36e
Amrit Krishnan <amrit110@gmail.com>
2024-09-25 08:10:05 -0400

    Add README, improve download script

--- README.md
+++ README.md
@@ -1,1 +1,113 @@
 # health-rec
+
+## Overview
+
+Welcome to the health-rec project! This project is designed to develop a recommendation system for health and community services.
+
+The project is built using a microservices architecture. It has the following components:
+
+- React frontend
+- Python backend
+- Chroma vector database
+- OpenAI for embedding and recommendation
+
+
+## Development
+
+### Prerequisites
+
+- Docker
+- Docker Compose
+- Node.js
+- Python
+- pip
+- npm
+
+### Setup
+
+1. Clone the repository
+
+2. Adding API keys
+
+In the `.env.development` file in the root of the project, set the following environment variables:
+  - OPENAI_API_KEY
+  - 211_API_KEY
+  - MAPBOX_API_KEY
+  - DATA_DIR
+  - COLLECTION_NAME
+
+Make sure to set appropriate values for these variables. The CHROMA_HOST, CHROMA_PORT, and COLLECTION_NAME
+are already defined in the file, but you may need to adjust their values if necessary.
+
+3. Install dependencies in a virtual environment for the backend
+
+```bash
+cd health_rec
+python3 -m venv .venv
+source .venv/bin/activate
+poetry install --with test
+```
+
+Now you can run pre-commit checks and tests:
+
+```bash
+pre-commit run --all-files
+```
+
+For development, we use docker compose to run the services.
+
+To run only the backend and chroma db services, run the following command:
+
+```bash
+docker compose --env-file .env.development -f docker-compose.dev.yml up
+```
+
+To run the frontend and backend, run the following command:
+
+```bash
+docker compose --env-file .env.development --profile frontend -f docker-compose.dev.yml up
+```
+
+4. Download data
+
+For example, to download GTA data, run the following command:
+
+```bash
+python scripts/download_data.py --api-key $YOUR_211_API_KEY --dataset on --is-gta --data-dir /mnt/data/211
+```
+
+To download Ontario-wide data, run the following command:
+
+```bash
+python scripts/download_data.py --api-key $YOUR_211_API_KEY --dataset on --data-dir /mnt/data/211
+```
+
+5. Upload data and embeddings
+
+First we use an interactive container:
+
+```bash
+docker run -it --network health-rec_app-network -v <data_dir_with_json_files>:/data -v `pwd`:/workspace -w /workspace vectorinstitute/health-rec:backend-dev-latest bash
+```
+
+Then we can run the following commands to upload the data to the vector database:
+
+```bash
+python3 health_rec/manage_data.py --create <collection_name>
+OPENAI_API_KEY=$YOUR_OPENAI_API_KEY python3 health_rec/manage_data.py --load <collection_name> --data-dir /data --load-embeddings
+python3 health_rec/manage_data.py --list
+```
+
+Careful while loading embeddings, it uses the OpenAI API, and hence make sure the data you want to use is correct. Test with a small amount of data first.
+
+6. Navigate to the UI on the browser
+
+```bash
+http://localhost:<frontend_port>
+```
+
+7. In case, you wish to update frontend dependencies, run the following commands in the `ui` directory:
+
+```bash
+npm install <package_name>
+```
--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,35 +1,18 @@
 """Download data from the 211 API."""
 
 import os
-from typing import Dict
 import requests
 from dotenv import load_dotenv
 import json
 import math
 from pathlib import Path
+import argparse
 
 
-load_dotenv("./.env.development")
-
-BASE_URL = "https://data.211support.org/api/v2/search"
-API_KEY = os.getenv("211_API_KEY")
-if not API_KEY:
-    raise ValueError("211_API_KEY is not set")
-
-PAGE_SIZE = 1000
-DATASET = "on"
-IS_GTA = True
-if IS_GTA:
-    DATA_DIR = "/mnt/data/211/gta"
-else:
-    DATA_DIR = f"/mnt/data/211/{DATASET}"
-os.makedirs(DATA_DIR, exist_ok=True)
-
-
-def create_payload(page_index: int) -> Dict[str, str]:
+def create_payload(page_index: int, dataset: str, is_gta: bool, page_size: int) -> dict:
     """Create the payload for the API request."""
     payload = {
-        "Dataset": DATASET,
+        "Dataset": dataset,
         "Lang": "en",
         "Search": "match",
         "MatchMode": "all",
@@ -37,11 +20,11 @@ def create_payload(page_index: int) -> Dict[str, str]:
         "Distance": 100000000000,
         "SortOrder": "distance",
         "PageIndex": page_index,
-        "PageSize": PAGE_SIZE,
+        "PageSize": page_size,
         "Fields": "TaxonomyTerm,TaxonomyTerms,TaxonomyCodes,Eligibility,FeeStructureSource,OfficialName,PhysicalCity,UniqueIDPriorSystem",
     }
 
-    if IS_GTA:
+    if is_gta:
         payload.update(
             {
                 "Communities": "Toronto|Durham Region|York Region|Peel Region",
@@ -60,39 +43,93 @@ def create_payload(page_index: int) -> Dict[str, str]:
     return payload
 
 
-def fetch_data(page_index: int) -> Dict[str, str]:
+def fetch_data(
+    page_index: int,
+    api_key: str,
+    base_url: str,
+    dataset: str,
+    is_gta: bool,
+    page_size: int,
+) -> dict:
     """Fetch data from the API for a given page index."""
     headers = {"Content-Type": "application/json"}
-    params = {"key": API_KEY}
+    params = {"key": api_key}
 
     response = requests.post(
-        BASE_URL, headers=headers, params=params, json=create_payload(page_index)
+        base_url,
+        headers=headers,
+        params=params,
+        json=create_payload(page_index, dataset, is_gta, page_size),
     )
     response.raise_for_status()
     return response.json()
 
 
-def save_to_file(data: Dict[str, str], file_name: str):
+def save_to_file(data: dict, file_name: str):
     """Save the data to a JSON file."""
     with open(file_name, "w") as f:
         json.dump(data, f, indent=2)
 
 
-def main():
-    first_page = fetch_data(0)
+def main(
+    api_key: str,
+    base_url: str,
+    dataset: str,
+    is_gta: bool,
+    data_dir: str,
+    page_size: int,
+):
+    os.makedirs(data_dir, exist_ok=True)
+
+    first_page = fetch_data(0, api_key, base_url, dataset, is_gta, page_size)
     total_records = first_page["RecordCount"]
-    total_pages = math.ceil(total_records / PAGE_SIZE)
+    total_pages = math.ceil(total_records / page_size)
 
     print(f"Total records: {total_records}")
     print(f"Total pages: {total_pages}")
 
     for page in range(total_pages):
         print(f"Fetching page {page + 1} of {total_pages}")
-        data = fetch_data(page)
+        data = fetch_data(page, api_key, base_url, dataset, is_gta, page_size)
         file_name = Path(f"data-{page:02d}.json")
-        save_to_file(data, os.path.join(DATA_DIR, file_name))
+        save_to_file(data, os.path.join(data_dir, file_name))
         print(f"Saved data to {file_name}")
 
 
 if __name__ == "__main__":
-    main()
+    load_dotenv("./.env.development")
+
+    parser = argparse.ArgumentParser(description="Download data from the 211 API.")
+    parser.add_argument(
+        "--api-key", default=os.getenv("211_API_KEY"), help="API key for 211 API"
+    )
+    parser.add_argument(
+        "--base-url",
+        default="https://data.211support.org/api/v2/search",
+        help="Base URL for 211 API",
+    )
+    parser.add_argument("--dataset", default="on", help="Dataset to download")
+    parser.add_argument(
+        "--is-gta", action="store_true", help="Whether to download GTA data"
+    )
+    parser.add_argument(
+        "--data-dir", default="/mnt/data/211", help="Directory to save data"
+    )
+    parser.add_argument(
+        "--page-size", type=int, default=1000, help="Number of records per page"
+    )
+
+    args = parser.parse_args()
+
+    if not args.api_key:
+        raise ValueError("211_API_KEY is not set")
+
+    data_dir = args.data_dir
+    if args.is_gta:
+        data_dir = os.path.join(data_dir, "gta")
+    else:
+        data_dir = os.path.join(data_dir, args.dataset)
+
+    main(
+        args.api_key, args.base_url, args.dataset, args.is_gta, data_dir, args.page_size
+    )
